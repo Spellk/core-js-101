08eb5983358fba6c9d01f41ff6c9e1fbea7592a9
Artem Lebedev <lebedevam91@mail.ru>
2023-06-29 18:05:20 +0300

    closures

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -85,8 +85,16 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cache = {};
+
+  return function memoizedFunc(...args) {
+    const argStr = JSON.stringify(args);
+    if (!cache[argStr]) {
+      cache[argStr] = func(...args);
+    }
+    return cache[argStr];
+  };
 }
 
 
@@ -151,8 +159,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function partialFunction(...args2) {
+    return fn.apply(this, args1.concat(args2));
+  };
 }
 
 
