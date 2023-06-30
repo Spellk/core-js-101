f435d75c2c1fa7b4ebbc1ca44efc4e54d4919fdf
Artem Lebedev <lebedevam91@mail.ru>
2023-06-30 16:48:33 +0300

    closures

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -183,8 +183,14 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let id = startFrom;
+
+  return function getNextId() {
+    const nextId = id;
+    id += 1;
+    return nextId;
+  };
 }
 
 
